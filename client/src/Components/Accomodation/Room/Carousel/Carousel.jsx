cf196107dca39e523810ba61af8fa887fe00a298
Ahnabu <horairaabu013025@gmail.com>
2024-09-19 13:34:53 +0600

    room modal finished

---
+++ client/src/Components/Accomodation/Room/Carousel/Carousel.jsx
@@ -0,0 +1,84 @@
+/* eslint-disable react/prop-types */
+import { Carousel, IconButton} from "@material-tailwind/react";
+
+
+export function CarouselCustomNavigation({ images}) {
+  
+    return (
+        <Carousel
+            transition={{ duration: 2 }}
+            className="rounded-xl py-4"
+            prevArrow={({ handlePrev }) => (
+                <IconButton
+
+                    size="lg"
+                    onClick={handlePrev}
+                    className="!absolute top-2/4 left-4 -translate-y-2/4 text-gold  bg-white bg-opacity-30"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                        />
+                    </svg>
+                </IconButton>
+            )}
+            nextArrow={({ handleNext }) => (
+                <IconButton
+
+                    size="lg"
+                    onClick={handleNext}
+                    className="!absolute top-2/4 !right-4 -translate-y-2/4 text-gold bg-white bg-opacity-30"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                        />
+                    </svg>
+                </IconButton>
+            )}
+
+
+            navigation={({ setActiveIndex, activeIndex, length }) => (
+                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                    {new Array(length).fill("").map((_, i) => (
+                        <span
+                            key={i}
+                            className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-gold" : "w-4 bg-white/50"
+                                }`}
+                            onClick={() => setActiveIndex(i)}
+                        />
+                    ))}
+                </div>
+            )}
+        >
+            {images.map((imageUrl, index) => (
+                <div key={index} className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+                    <img
+                        src={imageUrl}
+                        alt={`Gallery image ${index + 1}`}
+                        className="h-full w-full object-cover max-h-[80vh] 3xl:max-h-[700px] "
+                    />
+                </div>
+            ))}
+           
+        </Carousel>
+    );
+}
--- client/src/Components/Accomodation/Room/RoomModal.jsx
+++ client/src/Components/Accomodation/Room/RoomModal.jsx
@@ -3,14 +3,47 @@ import { useState } from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import { Modal } from "antd";
 import { useGetSingleRoomQuery } from "../../../redux/features/room/roomApi";
+import { CarouselCustomNavigation } from "./Carousel/Carousel";
 
 const RoomModal = ({ id }) => {
   const { data } = useGetSingleRoomQuery(id);
   const [isOpen, setIsOpen] = useState(false);
 
   const roomData = data?.data;
+
+  const {
+    room_overview,
+    special_benefits,
+    beds_and_bedding,
+    room_features,
+    bath_and_bathroom_features,
+    furniture_and_furnishings,
+    food_and_beverages,
+    internet_and_phones,
+    entertainment,
+    accessible_room_features,
+  } = roomData;
+
+  const Section = ({ title, children }) => (
+    <div className="mb-6">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p>
+        {children}
+      </p>
+      <hr className="my-3 md:mb-5 md:mt-4" />
+    </div>
+  );
+
+  const List = ({ items }) => (
+    <ul className="list-disc pl-5">
+      {items.map((item, index) => (
+        <li className="text-base" key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div>
+    <div className="max-w-screen-3xl text-start">
       <button
         onClick={() => setIsOpen(true)}
         className="inline-flex items-center transition-colors duration-300 hover:text-gold"
@@ -22,16 +55,101 @@ const RoomModal = ({ id }) => {
         width="90%"
         height="content"
         style={{
-          maxWidth: "100%",
-          border: "4px solid #111111",
+          maxWidth: "1560px",
+          border: "4px solid #fff395",
           borderRadius: "10px",
           padding: "0",
+
         }}
         open={isOpen}
         onCancel={() => setIsOpen(false)}
         footer={null}
       >
-        <h2>{roomData?.room_overview?.name}</h2>
+        <CarouselCustomNavigation images={roomData?.images} />
+        <div className="pt-6 md:p-4">
+
+          <h2 className="text-3xl pb-4">{roomData?.room_overview?.name}</h2>
+          <p className="text-base">
+            {roomData?.beds_and_bedding?.beds}
+          </p>
+          <div className="flex flex-wrap pb-3 md:pb-6">
+            <p className="">{room_overview.description}</p>
+            <p>, {room_overview.size}</p>
+            <p>{beds_and_bedding.beds}, {room_overview.wireless_internet}, {room_overview.coffee_tea_maker ? 'Coffee/tea maker.' : '.'}</p>
+          </div>
+          <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <div className="md:p-3">
+              <Section title="Room Overview">
+                <p>{room_overview.name}</p>
+              </Section>
+
+              <Section title="Special Benefits">
+                <List items={special_benefits} />
+              </Section>
+
+              <Section title="Beds and Bedding">
+                <p>Maximum occupancy: {beds_and_bedding.maximum_occupancy}</p>
+                <p>{beds_and_bedding.beds}</p>
+                <p>Rollaway beds: {beds_and_bedding.rollaway_beds_permitted ? 'Permitted' : 'Not permitted'}</p>
+                <p>Cribs permitted: {beds_and_bedding.cribs_permitted}</p>
+                {beds_and_bedding.duvet && <p>Duvet</p>}
+              </Section>
+
+              <Section title="Room Features">
+                <p>{room_features.air_conditioned ? 'Air-conditioned' : 'Non air-conditioned'}</p>
+                <p>{room_features.non_smoking ? 'This room is non-smoking' : 'Smoking allowed'}</p>
+                <p>Connecting rooms: {room_features.connecting_rooms_available ? 'Available' : 'Not available'}</p>
+                <p>{room_features.windows}</p>
+                {room_features.hooks && <p>Hooks</p>}
+                {room_features.usb_outlets && <p>USB outlets</p>}
+              </Section>
+
+            </div>
+              <div className="md:p-3">
+            <Section title="Bath and Bathroom Features">
+              <List items={Object.keys(bath_and_bathroom_features).filter(key => bath_and_bathroom_features[key] && key !== '_id').map(key => key.replace(/_/g, ' '))} />
+            </Section>
+
+            <Section title="Furniture and Furnishings">
+              <List items={Object.keys(furniture_and_furnishings).filter(key => furniture_and_furnishings[key] && key !== '_id' && key !== 'safe_fee').map(key => key.replace(/_/g, ' '))} />
+            </Section>
+
+            <Section title="Food & Beverages">
+              <p>Room service: {food_and_beverages.room_service}</p>
+              <p>Bottled water: {food_and_beverages.bottled_water}</p>
+              {food_and_beverages.coffee_tea_maker && <p>Coffee/tea maker</p>}
+              {food_and_beverages.instant_hot_water && <p>Instant hot water</p>}
+              <p>Minibar: {food_and_beverages.minibar}</p>
+            </Section>
+
+          </div>
+          <div className="md:p-3">
+            <Section title="Internet and Phones">
+              <p>Phones: {internet_and_phones.phones}</p>
+              <p>Phone features: {internet_and_phones.phone_features.join(', ')}</p>
+              <p>{internet_and_phones.wireless_internet}</p>
+            </Section>
+
+            <Section title="Entertainment">
+              {entertainment.plug_in_high_tech_room && <p>Plug-in, high-tech room</p>}
+              {entertainment.cable_satellite && <p>Cable/satellite</p>}
+              <p>International channels: {entertainment.international_channels.join(', ')}</p>
+            </Section>
+
+            <Section title="Accessible Room Features">
+              <p>Mobility accessible rooms: {accessible_room_features.mobility_accessible_rooms ? 'Available' : 'Not available'}</p>
+              <p>Roll-in shower: {accessible_room_features.roll_in_shower ? 'Available' : 'Not available'}</p>
+              <p>Hearing accessible rooms: {accessible_room_features.hearing_accessible_rooms ? 'Available' : 'Not available'}</p>
+            </Section>
+
+          </div>
+          </div>
+        
+
+
+
+        </div>
+
       </Modal>
     </div>
   );
