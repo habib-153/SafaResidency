6aaca52c1be88bec90e20ef04f344159f6da400d
Ahnabu <horairaabu013025@gmail.com>
2024-10-01 19:18:16 +0600

    staff routes ui complete, needs to work on functionality and load data form database

--- client/src/Dashboard/Staff/Housekeeping/Housekeeping.jsx
+++ client/src/Dashboard/Staff/Housekeeping/Housekeeping.jsx
@@ -3,17 +3,16 @@ import DPagination from "../../../Shared/Pagination";
 import "../../../Shared/style.css";
 import {
     useGetAllUsersQuery,
-  
+
 } from "../../../redux/features/auth/authApi";
 import Search from "../../../Components/ui/Search";
 import Loading from "../../../Components/ui/Loading";
-import { Button} from "antd";
-;
+import { Button, Dropdown, Space } from "antd";
 import { useState } from "react";
-import {  Dialog, DialogBody, DialogFooter, DialogHeader } from '@material-tailwind/react';
-const Reservation = () => {
+import { Dialog, DialogBody, DialogFooter, DialogHeader } from '@material-tailwind/react';
+const Housekeeping = () => {
     const { page, searchTerm } = useSelector((state) => state.filter);
-
+    const [roomId, setRoomId] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
     const { data, isLoading } = useGetAllUsersQuery([
         {
@@ -24,8 +23,36 @@ const Reservation = () => {
     ]);
   
 
-   
-  
+    const handleUpdateRole = async () => {
+        
+
+     
+    };
+
+    const items = [
+        {
+            label: "Admin",
+            key: "admin",
+        },
+        {
+            label: "User",
+            key: "user",
+        },
+        {
+            label: "Staff",
+            key: "staff",
+        },
+    ];
+
+    const menuProps = {
+        items,
+        onClick: handleUpdateRole,
+    };
+
+    //etar json rakha nai database e rekhe dish
+
+
+
 
     if (isLoading) return <Loading />;
     return (
@@ -33,8 +60,8 @@ const Reservation = () => {
             <div className="container mx-auto px-4 sm:px-8">
                 <div className="py-8">
                     <div className="text-center">
-                        <h1 className="text-2xl font-bold ">See all Reservations</h1>
-                        <p className="">See your all reservations here</p>
+                        <h1 className="text-2xl font-bold ">See all Rooms</h1>
+                        <p className="">See your all rooms here</p>
                         <div>
                             <Search searchPlaceholder="Search User" />
                         </div>
@@ -48,33 +75,29 @@ const Reservation = () => {
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
                                         >
-                                            User Name
+                                            Room Number
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
+                                        >
+                                            Room Id
                                         </th>
 
                                         <th
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
                                         >
-                                            User Email
+                                            Room Status
                                         </th>
 
-                                        <th
-                                            scope="col"
-                                            className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
-                                        >
-                                           Transaction ID
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
-                                        >
-                                           Room Number
-                                        </th>
+
+                                        
                                         <th
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b text-center border-gray-200 text-white  text-sm uppercase font-normal"
                                         >
-                                            Details
+                                            Requested on
                                         </th>
                                     </tr>
                                 </thead>
@@ -88,11 +111,18 @@ const Reservation = () => {
                                                 <td className="px-5 py-3">{user?.name}</td>
                                                 <td className="px-5 py-3">{user?.email}</td>
                                                 <td className="px-5 py-3">{user?.role}</td>
-                                                <td className="px-5 py-3">{user?.role}</td>
+                                             
                                                 <td className="px-5 py-1 text-center">
-                                                    <Button danger type="primary">
-                                                        Delete
-                                                    </Button>
+                                                    <Space>
+                                                        <Dropdown menu={menuProps} trigger={["click"]}>
+                                                            <Button onClick={() => setRoomId(user?._id)}>
+                                                                Update
+                                                            </Button>
+                                                        </Dropdown>
+                                                        <Button danger type="primary">
+                                                            Delete
+                                                        </Button>
+                                                    </Space>
                                                 </td>
                                             </tr>
                                         );
@@ -110,25 +140,25 @@ const Reservation = () => {
                 >
                     <DialogHeader className="text-gold">Confirm Guest Booking</DialogHeader>
                     <DialogBody divider className="grid gap-4">
-                        <p><span className="font-bold">Guest:</span> 
-                            
-                        
+                        <p><span className="font-bold">Guest:</span>
+
+
                         </p>
                         <p><span className="font-bold">Email:</span>
-                            
-                            
+
+
                         </p>
-                        <p><span className="font-bold">Room Category:</span> 
-                            
+                        <p><span className="font-bold">Room Category:</span>
+
                         </p>
-                        <p><span className="font-bold">Room Name:</span>  
-                            
+                        <p><span className="font-bold">Room Name:</span>
+
                         </p>
                         <p><span className="font-bold"> Room Number: </span> {data?.data?.room_overview?.room_number
                         }</p>
-                        <p><span className="font-bold">Check-in:</span> 
+                        <p><span className="font-bold">Check-in:</span>
                         </p>
-                        <p><span className="font-bold">Check-out:</span> 
+                        <p><span className="font-bold">Check-out:</span>
                         </p>
                         <p><span className="font-bold">Total Price:</span> $
                         </p>
@@ -142,7 +172,7 @@ const Reservation = () => {
                         >
                             Cancel
                         </Button>
-                     
+
                     </DialogFooter>
                 </Dialog>
             </div>
@@ -150,4 +180,4 @@ const Reservation = () => {
     );
 };
 
-export default Reservation;
+export default Housekeeping;
--- client/src/Dashboard/Staff/Requests/Requests.jsx
+++ client/src/Dashboard/Staff/Requests/Requests.jsx
@@ -3,18 +3,16 @@ import DPagination from "../../../Shared/Pagination";
 import "../../../Shared/style.css";
 import {
     useGetAllUsersQuery,
-  
+
 } from "../../../redux/features/auth/authApi";
 import Search from "../../../Components/ui/Search";
 import Loading from "../../../Components/ui/Loading";
-import { Button} from "antd";
-;
-import { useState } from "react";
-import {  Dialog, DialogBody, DialogFooter, DialogHeader } from '@material-tailwind/react';
-const Reservation = () => {
+import { Button } from "antd";
+
+const Requests = () => {
     const { page, searchTerm } = useSelector((state) => state.filter);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const { data, isLoading } = useGetAllUsersQuery([
         {
             name: "page",
@@ -22,10 +20,11 @@ const Reservation = () => {
         },
         { name: "searchTerm", value: searchTerm },
     ]);
-  
 
-   
-  
+    //json e data rakha ache oita dekhe update kore dish baki ta
+
+
+
 
     if (isLoading) return <Loading />;
     return (
@@ -33,8 +32,8 @@ const Reservation = () => {
             <div className="container mx-auto px-4 sm:px-8">
                 <div className="py-8">
                     <div className="text-center">
-                        <h1 className="text-2xl font-bold ">See all Reservations</h1>
-                        <p className="">See your all reservations here</p>
+                        <h1 className="text-2xl font-bold ">See all Requests</h1>
+                        <p className="">See your all requests here</p>
                         <div>
                             <Search searchPlaceholder="Search User" />
                         </div>
@@ -48,6 +47,12 @@ const Reservation = () => {
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
                                         >
+                                            Room Number
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
+                                        >
                                             User Name
                                         </th>
 
@@ -55,26 +60,21 @@ const Reservation = () => {
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
                                         >
-                                            User Email
+                                            User Requests
                                         </th>
 
+                                      
                                         <th
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
                                         >
-                                           Transaction ID
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-5 py-3 bg-gold border-b border-gray-200 text-white  text-left text-sm uppercase font-normal"
-                                        >
-                                           Room Number
+                                           Status
                                         </th>
                                         <th
                                             scope="col"
                                             className="px-5 py-3 bg-gold border-b text-center border-gray-200 text-white  text-sm uppercase font-normal"
                                         >
-                                            Details
+                                            Requested on
                                         </th>
                                     </tr>
                                 </thead>
@@ -103,51 +103,10 @@ const Reservation = () => {
                     </div>
                     <DPagination meta={data?.meta} />
                 </div>
-                <Dialog
-                    open={isModalOpen}
-                    handler={() => setIsModalOpen(false)}
-                    className="bg-[#F7F4ED] text-black"
-                >
-                    <DialogHeader className="text-gold">Confirm Guest Booking</DialogHeader>
-                    <DialogBody divider className="grid gap-4">
-                        <p><span className="font-bold">Guest:</span> 
-                            
-                        
-                        </p>
-                        <p><span className="font-bold">Email:</span>
-                            
-                            
-                        </p>
-                        <p><span className="font-bold">Room Category:</span> 
-                            
-                        </p>
-                        <p><span className="font-bold">Room Name:</span>  
-                            
-                        </p>
-                        <p><span className="font-bold"> Room Number: </span> {data?.data?.room_overview?.room_number
-                        }</p>
-                        <p><span className="font-bold">Check-in:</span> 
-                        </p>
-                        <p><span className="font-bold">Check-out:</span> 
-                        </p>
-                        <p><span className="font-bold">Total Price:</span> $
-                        </p>
-                    </DialogBody>
-                    <DialogFooter className="space-x-4">
-                        <Button
-                            variant="text"
-                            color="red"
-                            onClick={() => setIsModalOpen(false)}
-                            className="mr-1"
-                        >
-                            Cancel
-                        </Button>
-                     
-                    </DialogFooter>
-                </Dialog>
+                
             </div>
         </>
     );
 };
 
-export default Reservation;
+export default Requests;
