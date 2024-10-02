090304e1121ae63d9597c540b450d61bd42610b1
habib-153 <h.r.sihab155@gmail.com>
2024-10-02 14:46:31 +0600

    Refactor App.jsx, remove StickyNavbar component and update styling in Nav component

--- client/src/Shared/Header/BottomNav.jsx
+++ client/src/Shared/Header/BottomNav.jsx
@@ -1,124 +1,141 @@
-import { Button, Drawer } from "@material-tailwind/react";
+import { Button, Drawer, Typography } from "@material-tailwind/react";
 import { FaX, FaBars } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import NavbarProfile from "../NavbarProfile";
+import { useSelector } from "react-redux";
+import { currentUser } from "../../redux/features/auth/authSlice";
 
 const BottomNav = () => {
-    const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
+  const user = useSelector(currentUser);
 
-    const navLinks = <>
-        <Link to='/' className="text-secondary-blue">Home</Link>
-        <Link to='/appointment' className="text-secondary-blue">Appointment</Link>
-        {/* {
-            isAdmin ?
-                <Link to='/dashboard' className="text-secondary-blue">Dashboard</Link>
-                : isDoctor ? <Link to='/dashboard/doctorDashboard' className="text-secondary-blue">Dashboard</Link> :
-                    user && <Link to='/dashboard/user' className="text-secondary-blue">Dashboard</Link>
-        } */}
-        {/* {user && <Link to='/dashboard' className="text-secondary-blue">Dashboard</Link>} */}
-        <Link to='/tips' className="text-secondary-blue">Tips</Link>
-        <Link to='/doctors' className="text-secondary-blue">Doctors</Link>
-        <Link to='/services' className="text-secondary-blue">Services</Link>
-        <Link to='/aboutUs' className="text-secondary-blue">About</Link>
-        <Link to='/team' className="text-secondary-blue">Team</Link>
-        <Link to='/contact' className="text-secondary-blue">Contact US</Link>
-    </>
+  const list = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: " Accommodation",
+      path: "/accommodation",
+    },
+    {
+      name: "Dining",
+      path: "/dining",
+    },
+    {
+      name: "Gallery",
+      path: "/gallery",
+    },
+    {
+      name: "Meetings & Events",
+      path: "/events",
+    },
+  ];
 
-    // const authLinks = <>
-    //     <div className="flex flex-wrap gap-5">
-    //         <Link to='/register' >
-    //             <Button className="bg-secondary-blue text-sm normal-case">Registrater</Button>
-    //         </Link>
-    //         <Link to='/login' className="text-secondary-blue">
-    //             <Button className="border-secondary-blue text-sm border py-[10px] bg-transparent text-secondary-blue" >Login</Button>
-    //         </Link>
-    //     </div>
-    // </>
-
-    // const userLinks = <>
-    //     <div className="flex items-center justify-center">
-    //         <div className='relative group flex-col'>
-    //             <p className='mx-2 right-16 absolute group-hover:visible invisible text-black'>{user?.displayName}</p>
-    //             <div>
-    //                 <img className="w-10 h-10 rounded-full border-2 border-white hidden md:block mx-2" src={user?.photoURL} />
-    //             </div>
-    //         </div>
-    //         <button className='btn' onClick={handleLogout}>Logout</button>
-    //     </div>
-    // </>
-
-
-
-    return (
-        <nav className=" bg-white shadow-md py-2 mx-auto z-50 items-center">
-            <div className="max-w-[1536px] px-4 mx-auto flex justify-between items-center">
-                <ul className="font-bold hidden xl:flex gap-8 font-open-sans">
-                    {navLinks}
-                </ul>
-                <div className="md:hidden">
-                <Link
-                            to="/"
-                            className="flex items-center w-3/5 sm:w-1/3 z-50 logo-box gap-2 py-6"
-                        >
-                            <img loading="lazy" className="w-16" src='safa-logo.png' alt="VirtualDoc Logo" />
-                            <span className="text-3xl font-bold text-secondary-blue">
-                                Virtual
-                            </span>
-                            <span className="text-3xl font-bold text-primary-teal">
-                                Doc
-                            </span>
-                        </Link>
-                </div>
-
-                <FaBars
-                    onClick={() => setOpenNav(true)}
-                    className="block xl:hidden text-lg"
-                />
-                {/* logout added */}
-                <div className="md:flex items-center hidden ">
-                    <div className="float-right ">
-                        <NavbarProfile />
-                    </div>
-                </div>
-            </div>
-
-            <Drawer
-                placement="right"
-                open={openNav}
-                onClose={() => setOpenNav(false)}
-                className="p-4 fixed"
+  const navList = (
+    <ul className="mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 md:flex-row lg:items-center lg:text-lg  lg:gap-6 3xl:gap-9 font-semibold z-10 bg-white ">
+      {list.map((l) => {
+        return (
+          <Typography
+            key={l.name}
+            as="li"
+            variant="small"
+            color="black"
+            className="p-1 font-normal"
+          >
+            <NavLink
+              to={l.path}
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "3xl:text-2xl p-2 underline font-bold text-gold"
+                  : "text-black hover:text-gold transition-colors duration-500 3xl:text-2xl"
+              }
             >
-                <div className=" bg-white overflow-auto h-screen mx-auto z-50 items-center">
-                    <div className="flex bg-white justify-between items-center">
-                        <div className="flex items-center">
-                        <Link
-              to="/"
-              className="flex items-center w-3/5 sm:w-1/3 z-50 logo-box gap-2 py-6"
-            >
-              <img
-                loading="lazy"
-                className="w-16"
-                src='safa-logo.png'
-                alt="VirtualDoc Logo"
-              />
-              <span className="text-3xl font-bold text-secondary-blue">
-                Virtual
-              </span>
-              <span className="text-3xl font-bold text-primary-teal">Doc</span>
+              {l.name}
+            </NavLink>
+          </Typography>
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <nav className=" bg-white shadow-md py-2 mx-auto z-50 items-center">
+      <div className="max-w-[1536px] px-3 mx-auto flex justify-between items-center">
+        <ul className="font-bold hidden md:flex gap-8 font-open-sans">
+          {navList}
+        </ul>
+        <div className="md:hidden">
+          <Link to="/" className="z-50 logo-box">
+            <img
+              loading="lazy"
+              src="safa-logo.png"
+              className="w-24 2xl:h-24 h-14"
+              alt="safa logo"
+            />
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
+        {user ? (
+          <NavbarProfile></NavbarProfile>
+        ) : (
+          <div className="flex items-center gap-x-1">
+            <Link to={"/login"}>
+              <Button
+                className={`flex items-center text-primary justify-center w-full p-3 font-semibold tracking-wide rounded-md btn `}
+              >
+                Login
+              </Button>
             </Link>
-                        </div>
-                        <FaX onClick={() => setOpenNav(false)} className="text-lg" />
-                    </div>
+          </div>
+        )}
+        <FaBars
+          onClick={() => setOpenNav(true)}
+          className="block md:hidden text-lg"
+        />
+        </div>
+      </div>
 
-                    <ul className="flex  flex-col gap-2 font-bold text-secondary-blue font-open-sans py-3">
-                        {navLinks}
-                        <NavbarProfile />
-                    </ul>
-                </div>
-            </Drawer>
-        </nav>
-    );
+      <Drawer
+        placement="right"
+        open={openNav}
+        onClose={() => setOpenNav(false)}
+        className="p-4 fixed"
+      >
+        <div className=" bg-white overflow-auto h-screen mx-auto z-50 items-center">
+          <div className="flex bg-white justify-between items-center">
+            <div className="flex items-center">
+              <Link
+                to="/"
+                className="flex items-center w-3/5 sm:w-1/3 z-50 logo-box gap-2 py-6"
+              >
+                <img
+                  loading="lazy"
+                  className="w-16"
+                  src="safa-logo.png"
+                  alt="VirtualDoc Logo"
+                />
+                <span className="text-3xl font-bold text-secondary-blue">
+                  Virtual
+                </span>
+                <span className="text-3xl font-bold text-primary-teal">
+                  Doc
+                </span>
+              </Link>
+            </div>
+            <FaX onClick={() => setOpenNav(false)} className="text-lg" />
+          </div>
+
+          <ul className="flex  flex-col gap-2 font-bold text-secondary-blue font-open-sans py-3">
+            {navList}
+          </ul>
+        </div>
+      </Drawer>
+    </nav>
+  );
 };
 
 export default BottomNav;
--- client/src/Shared/Header/Nav.jsx
+++ client/src/Shared/Header/Nav.jsx
@@ -8,45 +8,41 @@ import StickyNav from "./StickyNav";
 const Nav = () => {
   return (
     <>
-      <header className="">
-        <nav className="relative hidden md:block bg-[#e9f2f2] px-4">
+      <header className="py-3">
+        <nav className="relative hidden md:block ">
           <span className="header-shape"></span>
-          <div className="container mx-auto flex justify-between">
+          <div className="max-w-screen-3xl mx-auto flex justify-between">
             <Link
               to="/"
-              className="flex items-center w-3/5 sm:w-1/3 z-50 logo-box gap-2 py-6"
+              className="flex items-center w-3/5 sm:w-1/3 z-50 logo-box gap-2"
             >
               <img
-                loading="lazy"
-                className="w-16"
-                src='safa-logo.png'
-                alt="VirtualDoc Logo"
-              />
-              <span className="text-3xl font-bold text-secondary-blue">
-                Virtual
-              </span>
-              <span className="text-3xl font-bold text-primary-teal">Doc</span>
+              loading="lazy"
+              src="safa-logo.png"
+              className="w-24 md:w-44 2xl:w-52 2xl:h-24 h-20"
+              alt="safa logo"
+            />
             </Link>
             <div className="w-2/5 sm:w-2/3 flex justify-end items-center gap-8">
               <a
-                href="mailto:mail.techtitans@gmail.com"
-                className="hidden sm:flex justify-center items-center gap-2 hover:text-primary-teal transition"
+                href="mailto:safa.residency.bd@gmail.com"
+                className="hidden sm:flex justify-center items-center gap-2 hover:text-gold transition"
               >
-                <FaRegEnvelopeOpen className="text-primary-teal" />
-                <p className="">help@virtualdoc.web.app</p>
+                <FaRegEnvelopeOpen className="text-gold" />
+                <p className="">safa.residency.bd@gmail.com</p>
               </a>
 
               <a
-                href="tel:+8801619152852"
-                className="hidden lg:flex justify-center items-center gap-2 hover:text-primary-teal transition"
+                href="tel:+8801234567899"
+                className="hidden lg:flex justify-center items-center gap-2 hover:text-gold transition"
               >
-                <MdOutlinePhone className="text-primary-teal text-lg" />
-                <p className="">+880 1619 152852</p>
+                <MdOutlinePhone className="text-gold text-lg" />
+                <p className="">+880 1111 123456</p>
               </a>
 
-              <Link to="/appointment">
-                <Button className="bg-secondary-blue rounded-full m-0 normal-case">
-                  Get Appointment
+              <Link to="/accommodation">
+                <Button className="bg-gold rounded-full m-0 normal-case">
+                  View Rates
                 </Button>
               </Link>
             </div>
