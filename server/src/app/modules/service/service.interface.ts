742c4f18a4b5a4088df442194fbbac72121b3b67
habib-153 <h.r.sihab155@gmail.com>
2024-10-14 05:04:37 +0600

    feat: Add service module
    Add service module with controller, model, route, and service files. The module includes functionality to create, retrieve, update, and delete services. It also includes validation and error handling.
    
    - Add service.controller.ts
    - Add service.interface.ts
    - Add service.model.ts
    - Add service.route.ts
    - Add service.service.ts
    
    Closes #123

---
+++ server/src/app/modules/service/service.interface.ts
@@ -0,0 +1,9 @@
+import { Types } from "mongoose"
+
+export type TService ={
+    room: Types.ObjectId;
+    user: Types.ObjectId;
+    service: string;
+    description: string;
+    isCompleted: boolean;
+}
