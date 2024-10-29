ea06b1824dd0a3326269ff32ef792ab10f14f429
habib-153 <h.r.sihab155@gmail.com>
2024-10-30 01:33:23 +0600

    Refactor package.json to add recharts dependency

--- client/src/routes/staffRoutes.jsx
+++ client/src/routes/staffRoutes.jsx
@@ -1,8 +1,8 @@
 import { RxDashboard } from "react-icons/rx";
 // import {FaUserPlus } from "react-icons/fa6"
-import { VscRequestChanges } from "react-icons/vsc"
+import { VscRequestChanges } from "react-icons/vsc";
 import { SiStatuspage } from "react-icons/si";
-import { TbReportAnalytics } from "react-icons/tb";
+//import { TbReportAnalytics } from "react-icons/tb";
 import { RiReservedFill } from "react-icons/ri";
 // import Booking from "../Components/Accommodation/Booking/Booking";
 import Profile from "../Dashboard/Profile/Profile";
@@ -11,69 +11,72 @@ import Requests from "../Dashboard/Staff/Requests/Requests";
 import RoomManagement from "../Dashboard/Admin/roomManagement/RoomManagement";
 
 export const staffPaths = [
-    {
-      label: (
-        <div className="flex items-center w-full">
-          <RxDashboard className="w-5 h-5" />
-          <span className="ml-4 font-medium rounded-lg">Dashboard</span>
-        </div>
-      ),
-      path: "dashboard",
-      element: <Profile />,
-    },
-    // {
-    //   label: (
-    //     <div className="flex items-center w-full">
-    //       <FaUserPlus className="w-5 h-5" />
-    //       <span className="ml-4 font-medium rounded-lg">Guest User</span>
-    //     </div>
-    //   ),
-       
-    //     path: "booking/:id",
-    //     element: <Booking />
-        
-     
-    // },
-    {
-      label: (
-        <div className="flex items-center w-full">
-          <VscRequestChanges className="w-5 h-5" />
-          <span className="ml-4 font-medium rounded-lg">Service Requests</span>
-        </div>
-      ),
-      path: "user-request",
-      element: <Requests/>,
-    },
+  {
+    label: (
+      <div className="flex items-center w-full">
+        <RxDashboard className="w-5 h-5" />
+        <span className="ml-4 font-medium rounded-lg">Dashboard</span>
+      </div>
+    ),
+    path: "dashboard",
+    element: <Profile />,
+  },
+  // {
+  //   label: (
+  //     <div className="flex items-center w-full">
+  //       <FaUserPlus className="w-5 h-5" />
+  //       <span className="ml-4 font-medium rounded-lg">Guest User</span>
+  //     </div>
+  //   ),
+
+  //     path: "booking/:id",
+  //     element: <Booking />
+
+  // },
+  {
+    label: (
+      <div className="flex items-center w-full">
+        <VscRequestChanges className="w-5 h-5" />
+        <span className="ml-4 font-medium rounded-lg">Service Requests</span>
+      </div>
+    ),
+    path: "user-request",
+    element: <Requests />,
+  },
   {
     label: (
       <div className="flex items-center w-full">
         <RiReservedFill className="w-5 h-5" />
-        <span className="ml-4 font-medium rounded-lg">Reservation Management</span>
+        <span className="ml-4 font-medium rounded-lg">
+          Reservation Management
+        </span>
       </div>
     ),
     path: "reservation",
-    element: <Reservation/> ,
-    },
-    {
-      label: (
-        <div className="flex items-center w-full">
-          <SiStatuspage className="w-5 h-5" />
-          <span className="ml-4 font-medium rounded-lg">Housekeeping and Maintenance</span>
-        </div>
-      ),
-      path: "housekeeping",
-      element: <RoomManagement />,
-    },
-    {
-      label: (
-        <div className="flex items-center w-full">
-          <TbReportAnalytics className="w-5 h-5" />
-          <span className="ml-4 font-medium rounded-lg">Report and Analytics</span>
-        </div>
-      ),
-      path: "report",
-      element: "f",
-    },
-  
-  ];
-  
+    element: <Reservation />,
+  },
+  {
+    label: (
+      <div className="flex items-center w-full">
+        <SiStatuspage className="w-5 h-5" />
+        <span className="ml-4 font-medium rounded-lg">
+          Housekeeping and Maintenance
+        </span>
+      </div>
+    ),
+    path: "housekeeping",
+    element: <RoomManagement />,
+  },
+  // {
+  //   label: (
+  //     <div className="flex items-center w-full">
+  //       <TbReportAnalytics className="w-5 h-5" />
+  //       <span className="ml-4 font-medium rounded-lg">
+  //         Report and Analytics
+  //       </span>
+  //     </div>
+  //   ),
+  //   path: "report",
+  //   element: "f",
+  // },
+];
