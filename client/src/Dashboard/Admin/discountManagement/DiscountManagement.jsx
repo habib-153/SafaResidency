967ca22fb22a5de77c0833751a413c445ea738b5
habib-153 <h.r.sihab155@gmail.com>
2024-11-10 05:30:44 +0600

    footer time changed, email template 80%chnged , coupon bug fixed, categories fixed

--- client/src/Dashboard/Admin/discountManagement/DiscountManagement.jsx
+++ client/src/Dashboard/Admin/discountManagement/DiscountManagement.jsx
@@ -1,6 +1,6 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { Button } from "@material-tailwind/react";
-import {  toast } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 import Loading from '../../../Components/ui/Loading';
 import { useCreateCouponMutation, useDeleteCouponMutation, useGetAllCouponsQuery } from '../../../redux/features/coupon/couponApi';
 import AddDiscountModal from './AddDiscountModal';
@@ -21,12 +21,13 @@ const DiscountManagement = () => {
   };
 
   const handleSaveCoupon = async (newCoupon) => {
+    const toastId = toast.loading('Creating coupon...');
     try {
       await createCoupon(newCoupon).unwrap();
-      toast.success('Coupon created successfully');
+      toast.success('Coupon created successfully', { id: toastId });
       handleCloseModal();
     } catch (error) {
-      toast.error('Failed to create coupon');
+      toast.error('Failed to create coupon', { id: toastId });
       console.log(error);
     }
   };
@@ -46,80 +47,79 @@ const DiscountManagement = () => {
 
   return (
     <div className="my-5 p-3">
-      
       <div className="mb-10 text-center">
-          <h1 className="text-3xl font-bold text-gray-900 mb-2">
-            <TagOutlined className="mr-2 text-gold" />
-            Coupon Management
-          </h1>
-          <p className="text-gray-600">Create and manage your discount coupons</p>
-        </div>
-        <div className="w-full flex justify-end mb-6">
-          <Button
-            type="primary"
-            onClick={handleOpenModal}
-            className="bg-gold hover:bg-gold border-gold hover:border-gold rounded-lg flex items-center gap-1"
-           
-          ><PlusOutlined />
-            Add New Coupon
-          </Button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {data?.data?.map((coupon) => (
-            <div key={coupon?.code} className="bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow duration-300 overflow-hidden">
-              <div className="p-6">
-                <div className="flex justify-between items-start mb-4">
-                  <div className="bg-gold px-4 py-2 rounded-lg">
-                    <h3 className="text-lg font-semibold text-white">{coupon?.code}</h3>
-                  </div>
-                  <div className={`px-3 py-1 rounded-full text-sm ${coupon?.isActive ? 'bg-green-50 text-green-700' : 'bg-gray-50 text-gray-700'}`}>
-                    {coupon.isActive ? 'Active' : 'Inactive'}
-                  </div>
+        <h1 className="text-3xl font-bold text-gray-900 mb-2">
+          <TagOutlined className="mr-2 text-gold" />
+          Coupon Management
+        </h1>
+        <p className="text-gray-600">Create and manage your discount coupons</p>
+      </div>
+      <div className="w-full flex justify-end mb-6">
+        <Button
+          type="primary"
+          onClick={handleOpenModal}
+          className="bg-gold hover:bg-gold border-gold hover:border-gold rounded-lg flex items-center gap-1"
+        >
+          <PlusOutlined />
+          Add New Coupon
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data?.data?.map((coupon) => (
+          <div key={coupon?.code} className="bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow duration-300 overflow-hidden">
+            <div className="p-6">
+              <div className="flex justify-between items-start mb-4">
+                <div className="bg-gold px-4 py-2 rounded-lg">
+                  <h3 className="text-lg font-semibold text-white">{coupon?.code}</h3>
                 </div>
-
-                <div className="space-y-3">
-                  <div className="flex justify-between items-center text-gray-600">
-                    <span>Discount:</span>
-                    <span className="font-medium text-gray-900">
-                      {coupon.discountType === 'percentage' ? `${coupon?.discountValue}%` : `$${coupon?.discountValue}`}
-                    </span>
-                  </div>
-                  
-                  <div className="flex justify-between items-center text-gray-600">
-                    <span>Min. Purchase:</span>
-                    <span className="font-medium text-gray-900">
-                      {coupon.minimumPurchaseAmount ? `$${coupon.minimumPurchaseAmount}` : 'None'}
-                    </span>
-                  </div>
-                  
-                  <div className="flex justify-between items-center text-gray-600">
-                    <span>Usage Limit:</span>
-                    <span className="font-medium text-gray-900">
-                      {coupon.usageLimit ? coupon.usageLimit : 'Unlimited'}
-                    </span>
-                  </div>
-                  
-                  <div className="flex justify-between items-center text-gray-600">
-                    <span>Expires:</span>
-                    <span className="font-medium text-gray-900">
-                      {coupon.expirationDate ? new Date(coupon.expirationDate).toLocaleDateString() : 'No expiry'}
-                    </span>
-                  </div>
-                </div>
-
-                <div className="mt-6 flex justify-end">
-                  <Button
-                    onClick={() => handleDeleteCoupon(coupon._id)}
-                    className="hover:bg-red-50 bg-red-50 rounded-lg flex items-center mr-4  border border-red-600 text-red-500"
-                    icon={<DeleteOutlined />}
-                  >
-                    Delete
-                  </Button>
+                <div className={`px-3 py-1 rounded-full text-sm ${coupon?.isActive ? 'bg-green-50 text-green-700' : 'bg-gray-50 text-gray-700'}`}>
+                  {coupon.isActive ? 'Active' : 'Inactive'}
                 </div>
               </div>
+
+              <div className="space-y-3">
+                <div className="flex justify-between items-center text-gray-600">
+                  <span>Discount:</span>
+                  <span className="font-medium text-gray-900">
+                    {coupon.discountType === 'percentage' ? `${coupon?.discountValue}%` : `$${coupon?.discountValue}`}
+                  </span>
+                </div>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <span>Min. Purchase:</span>
+                  <span className="font-medium text-gray-900">
+                    {coupon.minimumPurchaseAmount ? `$${coupon.minimumPurchaseAmount}` : 'None'}
+                  </span>
+                </div>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <span>Usage Limit:</span>
+                  <span className="font-medium text-gray-900">
+                    {coupon.usageLimit ? coupon.usageLimit : 'Unlimited'}
+                  </span>
+                </div>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <span>Expires:</span>
+                  <span className="font-medium text-gray-900">
+                    {coupon.expirationDate ? new Date(coupon.expirationDate).toLocaleDateString() : 'No expiry'}
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 flex justify-end">
+                <Button
+                  onClick={() => handleDeleteCoupon(coupon._id)}
+                  className="hover:bg-red-50 bg-red-50 rounded-lg flex items-center mr-4  border border-red-600 text-red-500"
+                  icon={<DeleteOutlined />}
+                >
+                  Delete
+                </Button>
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
 
       <AddDiscountModal
         visible={isModalVisible}
--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,25 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors({ origin: ['https://safaresidency.com', 'http://localhost:5173'], credentials: true}))
+
+const allowedOrigins = ['https://safaresidency.com', 'http://localhost:5173'];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+// app.use(cors({ origin: ['*'], credentials: true}))
+// app.use(cors({ origin: ['https://safaresidency.com', 'http://localhost:5173'], credentials: true}))
 
 //  application routes
 app.use('/api', router)
